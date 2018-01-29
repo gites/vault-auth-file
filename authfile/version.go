22c96f57d0bd2516903f8e99612716da34603c14
Tomasz Pawelczak <tpawelczak@fandom.com>
2018-01-29 16:09:11 +0100

    cleanups

--- authfile/version.go
+++ authfile/version.go
@@ -2,11 +2,14 @@ package authfile
 
 import "fmt"
 
+// Version build
 const Version = "0.0.1.dev"
 
 var (
-	Name      string
+	// Name of the program
+	Name string
+	// GitCommit hash
 	GitCommit string
-
+	//HumanVersion easy readable for Humans
 	HumanVersion = fmt.Sprintf("%s v%s (%s)", Name, Version, GitCommit)
 )
